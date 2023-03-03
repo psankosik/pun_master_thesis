9a9212e024ed324319d4d53ffedc170088ab1513
Pongpanod Sankosik <pongpanod.san@cjexpress.co.th>
2023-03-03 09:40:37 +0700

    fix

--- utils/read.py
+++ utils/read.py
@@ -5,7 +5,7 @@ def read_UCR_dataset_name(filepath=None, bakeoff=True, mySelection=3.0):
     df = pd.read_csv("DataSummary.csv")
     if bakeoff:
         df = df[df["BakeOff"] == True]
-        df = df[df["MySelection"] == mySelection]["Name"].to_list()
+        df = df[df["MySelection"] >= mySelection]["Name"].to_list()
         return df
     else:
         return df["Name"].to_list()
