ab2b00515165ffc1f2f235cff9092f1c138dbd7b
Mr. Pongpanod Sankosik <pongpanod.san.adm@192.168.5.87>
2023-06-21 21:57:40 +0700

    Feature: Add save and load augmented data

--- experiment/Experiment.py
+++ experiment/Experiment.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 from sktime.datasets import load_UCR_UEA_dataset
@@ -46,56 +47,125 @@ class Experiment:
             "y_test": y_test,
         }
 
-    def compile_preprocess(self):
-        if self.preprocess["name"] == "None":
-            return
+    # def compile_preprocess(self):
+    #     if self.preprocess == None:
+    #         return
 
-        if self.preprocess.get("params", None) == None:
-            self.dataset["x_train"] = self.preprocess["function"](
-                self.dataset["x_train"]
-            )
-            self.dataset["x_test"] = self.preprocess["function"](self.dataset["x_test"])
-        else:
-            self.dataset["x_train"] = self.preprocess["function"](
-                self.dataset["x_train"], **self.preprocess["params"]
-            )
-            self.dataset["x_test"] = self.preprocess["function"](
-                self.dataset["x_test"], **self.preprocess["params"]
-            )
+    #     if self.preprocess.get("name", None) == None:
+    #         return
 
-    def augmentation(self):
-        if self.augment["name"] == "None":
+    #     if self.preprocess.get("params", None) == None:
+    #         self.dataset["x_train"] = self.preprocess["function"](
+    #             self.dataset["x_train"]
+    #         )
+    #         self.dataset["x_test"] = self.preprocess["function"](self.dataset["x_test"])
+    #     else:
+    #         self.dataset["x_train"] = self.preprocess["function"](
+    #             self.dataset["x_train"], **self.preprocess["params"]
+    #         )
+    #         self.dataset["x_test"] = self.preprocess["function"](
+    #             self.dataset["x_test"], **self.preprocess["params"]
+    #         )
+
+
+        # def _load_augmented_result(dataset: str, augment_name: str, augment_params: dict, type: str) -> None:
+        #     augmented_dataset_folder = "Data_Augmented"
+        #     augmented_dataset_filename = f"{augmented_dataset_folder}/{dataset}_{augment_name}_{str(augment_params)}_{type}.npy"
+
+        #     if os.path.exists(augmented_dataset_filename):
+        #         # Load the augmented dataset
+        #         augmented_dataset = np.load(augmented_dataset_filename)
+        #     else:
+        #         # Perform the augmentation and save the augmented dataset
+        #         np.save(augmented_dataset_filename, augmented_dataset)
+
+
+    def apply_augmentation(self):
+        """
+        This method applies a given augmentation strategy to the dataset. 
+        """
+        def is_augmentation_required():
+            """
+            Checks if the augmentation strategy is set to None.
+            """
+            return self.augment["name"] != "None"
+
+        def set_augmented_data_to_original():
+            """
+            Sets the augmented data to be equal to the original data.
+            """
             self.dataset["x_train_aug"] = self.dataset["x_train"]
             self.dataset["x_test_aug"] = self.dataset["x_test"]
+
+        def get_reshaped_dataset():
+            """
+            Returns the reshaped train and test datasets.
+            """
+            return reshape_new_to_old_format(self.dataset["x_train"]), reshape_new_to_old_format(self.dataset["x_test"])
+
+        def perform_augmentation(x_train, x_test):
+            """
+            Performs the augmentation and returns the augmented data.
+            """
+            def get_augmented_dataset_filename(dataset_type):
+                augmented_dataset_folder = "Data_Augmented"
+                return f"{augmented_dataset_folder}/{self.dataset['name']}_{self.augment['name']}_{str(self.augment['params'])}_{dataset_type}.npy"
+
+            train_augmented_dataset_filename = get_augmented_dataset_filename("TRAIN")
+            test_augmented_dataset_filename = get_augmented_dataset_filename("TEST")
+
+            if os.path.exists(train_augmented_dataset_filename) and os.path.exists(test_augmented_dataset_filename):
+                # Load the augmented dataset
+                x_train_aug = np.load(train_augmented_dataset_filename)
+                x_test_aug = np.load(test_augmented_dataset_filename)
+            else:
+                if self.augment.get("enter_label"):
+                    x_train_aug = self.augment["function"](x_train, self.dataset["y_train"], **self.augment["params"])
+                    x_test_aug = self.augment["function"](x_test, self.dataset["y_test"], **self.augment["params"])
+                else:
+                    x_train_aug = self.augment["function"](x_train, **self.augment["params"])
+                    x_test_aug = self.augment["function"](x_test, **self.augment["params"])
+
+                np.save(train_augmented_dataset_filename, x_train_aug)
+                np.save(test_augmented_dataset_filename, x_test_aug)
+
+            return x_train_aug, x_test_aug
+
+        def should_concatenate_original():
+            """
+            Checks if the original data should be concatenated to the augmented data.
+            """
+            return self.augment.get("concat_original", False)
+
+        def concatenate_original_dataset(x_train_aug, x_test_aug):
+            """
+            Concatenates the original dataset to the augmented one and returns the result.
+            """
+            x_train_aug = np.concatenate((x_train_aug, reshape_new_to_old_format(self.dataset["x_train"])), axis=0)
+            x_test_aug = np.concatenate((x_test_aug, reshape_new_to_old_format(self.dataset["x_test"])), axis=0)
+            self.dataset["y_train"] = np.concatenate((self.dataset["y_train"], self.dataset["y_train"]), axis=0)
+            self.dataset["y_test"] = np.concatenate((self.dataset["y_test"], self.dataset["y_test"]), axis=0)
+            return x_train_aug, x_test_aug
+
+        def set_augmented_dataset(x_train_aug, x_test_aug):
+            """
+            Sets the augmented data in the dataset.
+            """
+            self.dataset["x_train_aug"] = reshape_old_to_new_format(x_train_aug)
+            self.dataset["x_test_aug"] = reshape_old_to_new_format(x_test_aug)
+
+        if not is_augmentation_required():
+            set_augmented_data_to_original()
             return
-        x_train = reshape_new_to_old_format(self.dataset["x_train"])
-        x_test = reshape_new_to_old_format(self.dataset["x_test"])
 
-        if self.augment.get("enter_label") == True:
-            x_train_aug = self.augment["function"](
-                x_train, self.dataset["y_train"], **self.augment["params"]
-            )
-            x_test_aug = self.augment["function"](
-                x_test, self.dataset["y_test"], **self.augment["params"]
-            )
+        x_train, x_test = get_reshaped_dataset()
+        x_train_aug, x_test_aug = perform_augmentation(x_train, x_test)
 
-        else:
-            x_train_aug = self.augment["function"](x_train, **self.augment["params"])
-            x_test_aug = self.augment["function"](x_test, **self.augment["params"])
+        if should_concatenate_original():
+            x_train_aug, x_test_aug = concatenate_original_dataset(x_train_aug, x_test_aug)
 
-        # Concat original or not
-        if self.augment.get("concat_original") == True:
-            x_train_aug = np.concatenate((x_train_aug, x_train), axis=0)
-            x_test_aug = np.concatenate((x_test_aug, x_test), axis=0)
-            self.dataset["y_train"] = np.concatenate(
-                (self.dataset["y_train"], self.dataset["y_train"]), axis=0
-            )
-            self.dataset["y_test"] = np.concatenate(
-                (self.dataset["y_test"], self.dataset["y_test"]), axis=0
-            )
+        set_augmented_dataset(x_train_aug, x_test_aug)
 
-        self.dataset["x_train_aug"] = reshape_old_to_new_format(x_train_aug)
-        self.dataset["x_test_aug"] = reshape_old_to_new_format(x_test_aug)
 
     def train_classier(self):
         if (self.dataset.get("x_train_aug").all() == None) or (
@@ -156,8 +226,8 @@ class Experiment:
 
     def run_all(self):
         self.load_UCR_dataset()
-        self.compile_preprocess()
-        self.augmentation()
+        # self.compile_preprocess()
+        self.apply_augmentation()
         self.train_classier()
         self.predict()
         self.evaluate()
